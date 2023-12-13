8c9d6637cca22de39499cf31ee24154f11fa56da
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-13 22:27:48 +0700

    s

--- ex-2.js
+++ ex-2.js
@@ -18,13 +18,18 @@ class EmailNotification extends Notification {
 }
 
 class SMSNotification extends Notification {
-  constructor(notificationID, createdTime, content, receiver) {
-    super(notificationID, createdTime, content, receiver);
+  constructor(notificationID, createdTime, content, phoneNumber) {
+    super(notificationID, createdTime, content);
+    this.phoneNumber = phoneNumber;
+  }
+
+  send() {
+    return `Notification has been sent to ${this.phoneNumber}`;
   }
 }
 
-const mailNoti = new EmailNotification(1, 1, 1, "Oliver");
-const smsNoti = new SMSNotification(1, 1, 1, "John");
+const mailNoti = new EmailNotification(1, 1, 1, "pipo@gmail.com");
+const smsNoti = new SMSNotification(1, 1, 1, "061-555-xxxx");
 
 console.log(mailNoti.send());
 console.log(smsNoti.send());
