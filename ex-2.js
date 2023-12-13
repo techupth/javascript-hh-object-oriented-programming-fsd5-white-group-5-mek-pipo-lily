db6ebe1426642d12174cab372d4d0261d08e0694
PIPOAUMPHOL <aumpholllo@gmail.com>
2023-12-13 21:54:55 +0700

    not yet

--- ex-2.js
+++ ex-2.js
@@ -5,8 +5,26 @@ class Notification {
     this.content = content;
     this.receiver = receiver;
   }
+
+  send() {
+    return `Notification has been sent to ${this.receiver}`;
+  }
 }
 
-class EmailNotification {}
+class EmailNotification extends Notification {
+  constructor(notificationID, createdTime, content, receiver) {
+    super(notificationID, createdTime, content, receiver);
+  }
+}
 
-class SMSNotification {}
+class SMSNotification extends Notification {
+  constructor(notificationID, createdTime, content, receiver) {
+    super(notificationID, createdTime, content, receiver);
+  }
+}
+
+const mailNoti = new EmailNotification(1, 1, 1, "Oliver");
+const smsNoti = new SMSNotification(1, 1, 1, "John");
+
+console.log(mailNoti.send());
+console.log(smsNoti.send());
