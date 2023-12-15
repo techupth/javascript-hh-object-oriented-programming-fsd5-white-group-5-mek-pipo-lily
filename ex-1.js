d61bd041a58f6b8b01ec0bad4c9f0a6462e053e3
NapatKan <147288328+NapatKan@users.noreply.github.com>
2023-12-15 16:49:08 +0700

    feat: add completed exercises

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,29 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send(email) {
+    console.log(`Notification has been sent to ${email}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send(phoneNumber) {
+    console.log(`Notification has been sent to ${phoneNumber}`);
+  }
+}
+
+const mailNotif = new EmailNotification();
+const smsNotif = new SMSNotification();
+
+mailNotif.send("mek@tumail.com");
+smsNotif.send("0123456789");
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,29 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content, receiver);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    super(notificationId, createdTime, content, phoneNumber);
+  }
+}
+
+const mailNotif = new EmailNotification("1", "2", "3", "cloud");
+const smsNotif = new SMSNotification("4", "5", "6", "moji");
+
+mailNotif.send();
+smsNotif.send();
