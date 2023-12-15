e46496172a1ee996ad563ce07a07daac4ba6c3b6
Yupaporn R. <lyupaporn.r@gmail.com>
2023-12-15 10:46:10 +0700

    feat(oop): add completed exercise

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,40 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
 
-class SMSNotification { }
+  send() {
+    console.log("Notification has been sent to " + this.receiver);
+  }
+}
+
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+
+  send() {
+    console.log("Notification has been sent to " + this.phoneNumber);
+  }
+}
+
+const emailNotification = new EmailNotification(
+  1,
+  "14-Dec-2023 4:00 PM",
+  "Hello, How are you?",
+  "Parksunghoon@belift.com"
+);
+const smsNotification = new SMSNotification(
+  1,
+  "14-Dec-2023 4:00 PM",
+  "Hello, How are you?",
+  "XXX-XXXXXXXX"
+);
+emailNotification.send();
+smsNotification.send();
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,44 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+  }
 
-class EmailNotification { }
+  send() {
+    console.log("Notification has been sent to " + this.receiver);
+  }
+}
 
-class SMSNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+}
+
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    super(notificationId, createdTime, content);
+    this.phoneNumber = phoneNumber;
+  }
+
+  send() {
+    console.log("Notification has been sent to " + this.phoneNumber);
+  }
+}
+
+const emailNotification = new EmailNotification(
+  1,
+  "14-Dec-2023 4:00 PM",
+  "Hello, How are you?",
+  "Parksunghoon@belift.com"
+);
+const smsNotification = new SMSNotification(
+  1,
+  "14-Dec-2023 4:00 PM",
+  "Hello, How are you?",
+  "XXX-XXXXXXXX"
+);
+emailNotification.send();
+smsNotification.send();
